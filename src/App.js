96d40e2cc45f9ea1f37bde5230780da646cfb7c3
Anil S. H <capitallatera@gmail.com>
2022-12-14 22:07:37 +0530

    rotated green light

--- src/App.js
+++ src/App.js
@@ -1,130 +1,106 @@
-import logo from "./logo.svg"
-import "./App.css"
-import { useEffect, useState } from "react"
-import Left from "./component/Left"
+import "./App.css";
+import { useState } from "react";
+import Left from "./component/Left";
 
-const timers = [7000, 7000, 7000, 7000]
-let counter = 0
+const timers = [7000, 7000, 7000, 7000];
+let counter = 0;
 
 const swtiching = (key) => {
-  switch (key) {
-    case 0:
-      return "top"
-    case 1:
-      return "right"
-    case 2:
-      return "bottom"
-    case 3:
-      return "left";
-    default: return 'top';
-  }
-}
+	switch (key) {
+		case 0:
+			return "top";
+		case 1:
+			return "right";
+		case 2:
+			return "bottom";
+		case 3:
+			return "left";
+		default:
+			return "top";
+	}
+};
 
 let nIntervId;
 function App() {
-  const [clockTurn, setClockTurn] = useState('');
-  const [store, setStore] = useState('');
+	const [clockTurn, setClockTurn] = useState("");
 
-  function traffic() {
-    // console.log(swtiching(counter), counter, 'Working');
-    setClockTurn(swtiching(counter))
-    if (counter <= 3) return (counter += 1)
-    counter = 0
-    // stopLights();
-    // console.log("It's Stopped");
-  }
+	function traffic() {
+		setClockTurn(swtiching(counter));
+		if (counter <= 3) return (counter += 1);
+		counter = 0;
+    stopLights();
+	}
 
-  function changeLights() {
-    if (clockTurn === 'shut') setClockTurn(store);
-    else traffic();
-    
-    if (!nIntervId) {
-      nIntervId = setInterval(traffic, timers[counter])
-      
-    }
-  }
+	function changeLights() {
+		if (!nIntervId) {
+			nIntervId = setInterval(traffic, timers[counter]);
+		}
+	}
 
-  function stopLights() {
-    clearInterval(nIntervId)
-    nIntervId = null
-    setClockTurn(value => {
-      setStore(value);
-      return 'shut'
-    } );
-  }
-  console.log(clockTurn, 'clockTurn');
-  return (
-    <div className="relative">
-      <div className="top" style={styling.top}>
-        <Left  styling ={{    
-          display: 'flex',
-          width: 'fit-content',
-          flexDirection: 'row-reverse',
-        }} 
-        check={clockTurn === 'top'}
-        clockTurn={clockTurn}
-        divStyle={{margin: '0px 5px'}}
-        initialValue = {counter === 0 ? 'green' : 'red'}
-      />
-      </div>
+	function stopLights() {
+		clearInterval(nIntervId);
+		nIntervId = null;
+	}
+  console.log(counter, 'counter')
+	return (
+		<div className="relative">
+			<div className="top" style={styling.top}>
+				<Left styling={styling.topLeft} divStyle={styling.divStyling} active={counter ===0} initial={counter} clockTurn={clockTurn}/>
+			</div>
 
-      <div style={styling.middle}>
-        <div className="left">
-        <Left check={clockTurn === 'left'}
-        clockTurn={clockTurn}
-        initialValue = {counter === 1 ? 'green' : 'red'}/>
-      </div>
+			<div style={styling.middle}>
+				<div className="left">
+					<Left active={counter === 3} initial={counter} clockTurn={clockTurn}/>
+				</div>
 
-      <div className="right">
-        <Left 
-          styling={{
-            flexDirection: 'column-reverse',
-            display: 'flex',
-          }}
-        
-        check={clockTurn === 'right'}
-        clockTurn={clockTurn}
-        initialValue = {counter === 2 ? 'green' : 'red'}/>
-      </div>
-      </div>
+				<div className="right">
+					<Left styling={styling.rightLeft} active={counter ===1} initial={counter} clockTurn={clockTurn}/>
+				</div>
+			</div>
 
-      <div className="bottom" style={styling.bottom}>
-        <Left styling ={{    
-          display: 'flex',
-          width: 'fit-content'}} 
-          check={clockTurn === 'bottom'}
-          clockTurn={clockTurn}
-          divStyle={{margin: '0px 5px'}}
-          initialValue = {counter === 3 ? 'green' : 'red'}
-        />
-      </div>
-      <div className="buttons" style={styling.top}>
-      <button id="start" onClick={() => changeLights()}>
-        Start
-      </button>
-      <button id="stop" onClick={() => stopLights()}>
-        Stop
-      </button>
-      </div>
-    </div>
-  )
+			<div className="bottom" style={styling.bottom}>
+				<Left styling={styling.bottomLeft} divStyle={styling.divStyling} active={counter ===2} initial={counter} clockTurn={clockTurn}/>
+			</div>
+			<div className="buttons" style={styling.top}>
+				<button id="start" onClick={() => changeLights()}>
+					Start
+				</button>
+				<button id="stop" onClick={() => stopLights()}>
+					Stop
+				</button>
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
 
 const styling = {
-  top : {
-    display: 'flex',
-    justifyContent: 'center',
-  },
+	top: {
+		display: "flex",
+		justifyContent: "center",
+	},
 
-  bottom : {
-    display: 'flex',
-    justifyContent: 'center',
-  },
-  middle : {
-    display: 'flex',
-    justifyContent: 'space-around',
-  }
-}
-
+	bottom: {
+		display: "flex",
+		justifyContent: "center",
+	},
+	middle: {
+		display: "flex",
+		justifyContent: "space-around",
+	},
+	topLeft: {
+		display: "flex",
+		width: "fit-content",
+		flexDirection: "row-reverse",
+	},
+	rightLeft: {
+		flexDirection: "column-reverse",
+		display: "flex",
+	},
+	bottomLeft: {
+		display: "flex",
+		width: "fit-content",
+	},
+	divStyling: { margin: "0px 5px" },
+};
--- src/component/Left.jsx
+++ src/component/Left.jsx
@@ -1,62 +1,68 @@
-import "../App.css"
-import { useEffect, useState } from "react"
-const timers = [2000, 2000, 2000]
-let counter = 0
+import "../App.css";
+import { useEffect, useState } from "react";
+const timers = [1000, 1000, 1000];
+let counter = 0;
 const swtiching = (key) => {
-  switch (key) {
-    case 0:
-      return "green"
-    case 1:
-      return "yellow"
-    case 2:
-      return "red"
-    default:
-      return "red"
-  }
-}
+	switch (key) {
+		case 0:
+			return "green";
+		case 1:
+			return "yellow";
+		case 2:
+			return "red";
+		default:
+			return "red";
+	}
+};
 let nIntervId;
-function Left({styling, check, clockTurn, divStyle, initialValue}) {
-  const [turn, setTurn] = useState(check ? 'green' : 'red');
-  function traffic() {
-    // console.log(swtiching(counter+1), counter, 'Switching Lights');
-    setTurn(swtiching(counter+1))
-    if (counter < 2) return (counter += 1)
-    counter = 0
+function Left({ styling, divStyle, active, initial, clockTurn }) {
+	const [turn, setTurn] = useState("red");
+	function traffic() {
+		setTurn(swtiching(counter));
+		if (counter < 2) return (counter += 1);
+		counter = 0;
     stopTextColor();
-  }
-  const changeColor = () => {
-    if (!nIntervId) {
-      nIntervId = setInterval(traffic, timers[counter])
-    }
-  }
-  function stopTextColor() {
-    clearInterval(nIntervId)
-    nIntervId = null
-  }
-  useEffect(()=>{
-    if(check) return changeColor();
-    return ()=> stopTextColor();
-  },[check]);
-
+	}
+	const changeColor = () => {
+		if (!nIntervId) {
+			nIntervId = setInterval(traffic, timers[counter]);
+		}
+	};
+	function stopTextColor() {
+		clearInterval(nIntervId);
+		nIntervId = null;
+	}
   useEffect(() =>{
-    if(clockTurn === 'shut') return stopTextColor();
-    if(check && (clockTurn !== 'shut') && (counter === 0)) return setTurn(swtiching(0))
-  },[check, clockTurn])
-  // console.log(check, 'checkcheck');
-  return (
-    <div id="isiqfor" style={styling}>
-      <div className={`red ${turn == "red" ? "on" : ""}`} style={divStyle}></div>
-      <div className={`yellow ${turn == "yellow" ? "on" : ""}`} style={divStyle}></div>
-      <div className={`green ${turn == "green" ? "on" : ""}`} style={divStyle}></div>
-      {/* <button id="start" onClick={() => changeColor()}>
-        Start
-      </button>
-      <button id="stop" onClick={() => stopTextColor()}>
-        Stop
-      </button> */}
-    </div>
-  )
+    setTurn(active ? "green" : "red")
+  },[active]);
+
+  // useEffect(()=>{
+  //   if(active) return changeColor();
+  //   return ()=> stopTextColor();
+  // },[active]);
+  // console.log(active && initial, turn, 'initialinitial');
+	return (
+		<div id="isiqfor" style={styling}>
+			<div
+				className={`red ${turn === "red" ? "on" : ""}`}
+				style={divStyle}
+			></div>
+			<div
+				className={`yellow ${turn === "yellow" ? "on" : ""}`}
+				style={divStyle}
+			></div>
+			<div
+				className={`green ${turn === "green" ? "on" : ""}`}
+				style={divStyle}
+			></div>
+			<button id="start" onClick={() => changeColor()}>
+				Start
+			</button>
+			<button id="stop" onClick={() => stopTextColor()}>
+				Stop
+			</button>
+		</div>
+	);
 }
 
-export default Left
-
+export default Left;
